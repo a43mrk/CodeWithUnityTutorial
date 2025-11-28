e22baee5f6daf6f605a044415f681e2966ae243b
r1ck5 <ricardosasaki1@gmail.com>
2025-11-28 16:33:17 +0300

    Challenge1 done

---
+++ Prototype1/Assets/Challenge 1/Scripts/SpinOnZAxis.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SpinOnZAxis : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back, 1000 * Time.deltaTime);
+    }
+}

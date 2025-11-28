97e3839e82b9ae4638ccaa1bf0fe26e9aad8ba72
r1ck5 <ricardosasaki1@gmail.com>
2025-11-28 14:03:01 +0300

    adds turnspeed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,7 +15,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward
-        // transform.Translate(0, 0, 1);
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }

7fa0821927c2da27d9e1270a651171f07e615d78
r1ck5 <ricardosasaki1@gmail.com>
2025-11-30 21:46:55 +0300

    spawn animals randonly when pressing s

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 16;
+    private float spawnPosZ = 20;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            // Randomly generate animal index and spawn position
+            var prefab = animalPrefabs[animalIndex];
+            Vector3 spawnPos = new(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            Instantiate(prefab,
+                spawnPos,
+                prefab.transform.rotation
+            );
+        }
+    }
+}

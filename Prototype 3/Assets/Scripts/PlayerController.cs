a80e96af1b0842db996794102f6b52099e57e6a3
r1ck5 <ricardosasaki1@gmail.com>
2025-12-02 18:53:30 +0300

    player can jump

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float jumpForce = 100f;
+    public float gravityModifier;
+    private Rigidbody playerRb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }

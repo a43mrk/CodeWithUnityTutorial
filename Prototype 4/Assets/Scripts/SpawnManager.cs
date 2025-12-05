c5ff9d5fc390df127f3858f574daf0d6fb476a46
r1ck5 <ricardosasaki1@gmail.com>
2025-12-05 18:41:45 +0300

    Destroy enemies if they fall off

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -3,23 +3,35 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public int enemyCount;
     private float spawnRange = 9f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        SpawnEnemyWave(3);
+    }
 
-        Instantiate(
-            enemyPrefab,
-            GenerateSpawnPosition(),
-            enemyPrefab.transform.rotation
-            );
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(
+                enemyPrefab,
+                GenerateSpawnPosition(),
+                enemyPrefab.transform.rotation
+                );
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if(enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()

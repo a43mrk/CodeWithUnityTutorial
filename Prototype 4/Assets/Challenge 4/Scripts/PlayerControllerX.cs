233f719f02059d42b315ec849c1bb961e5c9bead
r1ck5 <ricardosasaki1@gmail.com>
2025-12-05 20:09:17 +0300

    The player has a turbo boost

--- Prototype 4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Prototype 4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -14,6 +14,7 @@ public class PlayerControllerX : MonoBehaviour
 
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
+    public ParticleSystem speedBoostFx;
     
     void Start()
     {
@@ -23,9 +24,19 @@ public class PlayerControllerX : MonoBehaviour
 
     void Update()
     {
-        // Add force to player in direction of the focal point (and camera)
-        float verticalInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            speedBoostFx.Play();
+            // Add force to player in direction of the focal point (and camera)
+            float verticalInput = Input.GetAxis("Vertical");
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime, ForceMode.Impulse); 
+        }
+        else
+        {
+            // Add force to player in direction of the focal point (and camera)
+            float verticalInput = Input.GetAxis("Vertical");
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+        }
 
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);

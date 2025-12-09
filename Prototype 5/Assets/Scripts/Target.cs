da323a2aa02f526fe9556dcf0b77876344714071
r1ck5 <ricardosasaki1@gmail.com>
2025-12-09 14:24:07 +0300

    add explosion on tap of items

--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -11,6 +11,7 @@ public class Target : MonoBehaviour
     private GameManager gameManager;
 
     public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -58,6 +59,7 @@ public class Target : MonoBehaviour
     void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
         gameManager.UpdateScore(pointValue);
     }
 

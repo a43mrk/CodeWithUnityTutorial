c240f5b0ffc274b3b25560d19c4d14b17892603f
r1ck5 <ricardosasaki1@gmail.com>
2025-12-09 12:42:03 +0300

    Destroy target with click and sensor

--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -44,4 +44,14 @@ public class Target : MonoBehaviour
     {
         
     }
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
 }

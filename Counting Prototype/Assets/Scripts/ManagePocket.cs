f5ce8052b7f1690bad801e2fd653ce5d21726604
r1ck5 <ricardosasaki1@gmail.com>
2025-12-22 21:35:21 +0300

    fix some prefabs

--- Counting Prototype/Assets/Scripts/ManagePocket.cs
+++ Counting Prototype/Assets/Scripts/ManagePocket.cs
@@ -6,12 +6,13 @@ public class ManagePocket : MonoBehaviour
     public GameObject rightArm;
     public float openAngle = 45.0f;
     private GameManager gameManager;
+    private bool isClosed = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        leftArm.transform.Rotate(Vector3.back * 5);
-        rightArm.transform.Rotate(Vector3.forward * 5);
+        leftArm.transform.Rotate(Vector3.down * 5);
+        rightArm.transform.Rotate(Vector3.up * 5);
 
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
@@ -21,19 +22,22 @@ public class ManagePocket : MonoBehaviour
     {
         if(gameManager.IsJackpotTime())
         {
-            OpenArms();
+            if(isClosed)
+                OpenArms();
         }
     }
 
     public void OpenArms()
     {
-        leftArm.transform.Rotate(Vector3.back * openAngle);
-        rightArm.transform.Rotate(Vector3.forward * openAngle);
+        isClosed = false;
+        leftArm.transform.Rotate(Vector3.down * openAngle);
+        rightArm.transform.Rotate(Vector3.up * openAngle);
     }
 
     public void CloseArms()
     {
-        leftArm.transform.Rotate(Vector3.forward * openAngle);
-        rightArm.transform.Rotate(Vector3.back * openAngle);
+        isClosed = true;
+        leftArm.transform.Rotate(Vector3.up * openAngle);
+        rightArm.transform.Rotate(Vector3.down * openAngle);
     }
 }
